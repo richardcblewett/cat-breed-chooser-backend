c944ed03738dbfc852748c741038b13f1db1fee8
Richard Blewett <richardcblewett@gmail.com>
2022-02-05 11:19:11 -0500

    modifying jpa for searching is complete.

--- src/main/java/com/catbreedchooser/catbreedchooserbackend/repository/BreedRepository.java
+++ src/main/java/com/catbreedchooser/catbreedchooserbackend/repository/BreedRepository.java
@@ -2,17 +2,16 @@ package com.catbreedchooser.catbreedchooserbackend.repository;
 
 import com.catbreedchooser.catbreedchooserbackend.model.Breed;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public interface BreedRepository extends JpaRepository<Breed, Long> {
     boolean existsByName(String name);
-
-    List<Breed> findByNameNotNull();//returns all breeds
-    List<Breed> findByChildFriendlyIsGreaterThanEqualAndIntelligenceIsGreaterThanEqualAndGroomingIsGreaterThanEqual(Long childFriendly, Long intelligence, Long grooming);
+    List<Breed> findByNameNotNullAndIdNotNull();
+    List<Breed> findByChildFriendlyIsGreaterThanEqualAndIntelligenceIsGreaterThanEqualAndGroomingIsLessThanEqual(Long childFriendly, Long intelligence, Long grooming);
     Breed findById(String id);
 }
