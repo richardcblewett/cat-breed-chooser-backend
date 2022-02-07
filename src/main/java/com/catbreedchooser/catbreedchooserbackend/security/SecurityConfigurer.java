12c108d53f87709c5ecf1541b247cb4064e5da25
Richard Blewett <richardcblewett@gmail.com>
2022-02-07 15:17:37 -0500

    taking care of ide warnings

--- src/main/java/com/catbreedchooser/catbreedchooserbackend/security/SecurityConfigurer.java
+++ src/main/java/com/catbreedchooser/catbreedchooserbackend/security/SecurityConfigurer.java
@@ -1,20 +1,9 @@
 package com.catbreedchooser.catbreedchooserbackend.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.context.WebApplicationContext;
 
 @EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
